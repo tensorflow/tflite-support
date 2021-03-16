d0b24231d619da7628a5de9ac208cf96a0289717
George <farmaker47@gmail.com>
2021-03-16 19:45:28 +0200

    Change indentation to 2

--- tensorflow_lite_support/java/src/java/org/tensorflow/lite/support/image/ops/TransformToGrayscaleOp.java
+++ tensorflow_lite_support/java/src/java/org/tensorflow/lite/support/image/ops/TransformToGrayscaleOp.java
@@ -28,112 +28,112 @@ import org.tensorflow.lite.support.image.ImageOperator;
 import org.tensorflow.lite.support.image.TensorImage;
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
 import org.tensorflow.lite.DataType;
+
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
 /**
  * Transforms an RGB image to GrayScale as a image processing unit.
- *
+ * <p>
  * The conversion is based on OpenCV RGB to GRAY conversion
  * https://docs.opencv.org/master/de/d25/imgproc_color_conversions.html#color_convert_rgb_gray
- *
+ * <p>
  * The 3 channels of the grayscale image are identical and one of them is used to create the TensorBuffer
- *
  */
 public class TransformToGrayscaleOp implements ImageOperator {
 
-    /**
-     * Creates a TransformToGrayscaleOp.
-     */
-    public TransformToGrayscaleOp() {
-    }
+  /**
+   * Creates a TransformToGrayscaleOp.
+   */
+  public TransformToGrayscaleOp() {
+  }
 
-    /**
-     * Applies the defined transformation to grayscale and returns a TensorImage.
-     *
-     * Because the 3 channels of the image are identical only one is used to create the TensorBuffer
-     *
-     * @param image input image.
-     * @return output image.
-     */
-    @Override
-    @NonNull
-    public TensorImage apply(@NonNull TensorImage image) {
-        int height = image.getBitmap().getHeight();
-        int width = image.getBitmap().getWidth();
-        Bitmap bmpGrayscale = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(bmpGrayscale);
-        Paint paint = new Paint();
-        // A matrix is created that will be applied to canvas to generate grayscale image
-        // The luminance of each pixel is calculated as the weighted sum of the 3 RGB values
-        // Y = 0.299R + 0.587G + 0.114B
-        float[] matrix = new float[]{0.299F, 0.587F, 0.114F, 0.0F, 0.0F,
-                                     0.299F, 0.587F, 0.114F, 0.0F, 0.0F,
-                                     0.299F, 0.587F, 0.114F, 0.0F, 0.0F,
-                                     0.0F, 0.0F, 0.0F, 1.0F, 0.0F};
-        ColorMatrixColorFilter colorMatrixFilter = new ColorMatrixColorFilter(matrix);
-        paint.setColorFilter((ColorFilter) colorMatrixFilter);
-        canvas.drawBitmap(image.getBitmap(), 0.0F, 0.0F, paint);
+  /**
+   * Applies the defined transformation to grayscale and returns a TensorImage.
+   * <p>
+   * Because the 3 channels of the image are identical only one is used to create the TensorBuffer
+   *
+   * @param image input image.
+   * @return output image.
+   */
+  @Override
+  @NonNull
+  public TensorImage apply(@NonNull TensorImage image) {
+    int height = image.getBitmap().getHeight();
+    int width = image.getBitmap().getWidth();
+    Bitmap bmpGrayscale = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+    Canvas canvas = new Canvas(bmpGrayscale);
+    Paint paint = new Paint();
+    // A matrix is created that will be applied to canvas to generate grayscale image
+    // The luminance of each pixel is calculated as the weighted sum of the 3 RGB values
+    // Y = 0.299R + 0.587G + 0.114B
+    float[] matrix = new float[]{0.299F, 0.587F, 0.114F, 0.0F, 0.0F,
+            0.299F, 0.587F, 0.114F, 0.0F, 0.0F,
+            0.299F, 0.587F, 0.114F, 0.0F, 0.0F,
+            0.0F, 0.0F, 0.0F, 1.0F, 0.0F};
+    ColorMatrixColorFilter colorMatrixFilter = new ColorMatrixColorFilter(matrix);
+    paint.setColorFilter((ColorFilter) colorMatrixFilter);
+    canvas.drawBitmap(image.getBitmap(), 0.0F, 0.0F, paint);
 
-        // Get the pixels from the generated grayscale image
-        int w = bmpGrayscale.getWidth();
-        int h = bmpGrayscale.getHeight();
-        int[] intValues = new int[w * h];
-        bmpGrayscale.getPixels(intValues, 0, w, 0, 0, w, h);
-        // Shape with one channel
-        int[] shape = new int[]{1, h, w, 1};
+    // Get the pixels from the generated grayscale image
+    int w = bmpGrayscale.getWidth();
+    int h = bmpGrayscale.getHeight();
+    int[] intValues = new int[w * h];
+    bmpGrayscale.getPixels(intValues, 0, w, 0, 0, w, h);
+    // Shape with one channel
+    int[] shape = new int[]{1, h, w, 1};
 
-        switch (image.getDataType()) {
-            case UINT8:
-                // Create byte array and use one of the 3 identical channels
-                byte[] byteArr = new byte[w * h * 1];
-                for (int i = 0, j = 0; i < intValues.length; i++) {
-                    byteArr[j++] = (byte) (intValues[i] & 0xff);
-                }
-                ByteBuffer byteBuffer = ByteBuffer.allocateDirect(1 * w * h * 1 * 1);
-                byteBuffer.order(ByteOrder.nativeOrder());
-                byteBuffer.put(byteArr);
+    switch (image.getDataType()) {
+      case UINT8:
+        // Create byte array and use one of the 3 identical channels
+        byte[] byteArr = new byte[w * h * 1];
+        for (int i = 0, j = 0; i < intValues.length; i++) {
+          byteArr[j++] = (byte) (intValues[i] & 0xff);
+        }
+        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(1 * w * h * 1 * 1);
+        byteBuffer.order(ByteOrder.nativeOrder());
+        byteBuffer.put(byteArr);
 
-                TensorBuffer buffer = TensorBuffer.createFixedSize(shape, DataType.UINT8);
-                buffer.loadBuffer(byteBuffer, shape);
+        TensorBuffer buffer = TensorBuffer.createFixedSize(shape, DataType.UINT8);
+        buffer.loadBuffer(byteBuffer, shape);
 
-                image.load(buffer, GRAYSCALE);
+        image.load(buffer, GRAYSCALE);
 
-                break;
-            case FLOAT32:
-                // Create float array and use one of the 3 identical channels
-                float[] floatArr = new float[w * h * 1];
-                for (int i = 0, j = 0; i < intValues.length; i++) {
-                    floatArr[j++] = (float) (intValues[i] & 0xff);
-                }
-
-                TensorBuffer bufferFloat = TensorBuffer.createFixedSize(shape, DataType.FLOAT32);
-                bufferFloat.loadArray(floatArr, shape);
-
-                image.load(bufferFloat, GRAYSCALE);
-
-                break;
-            default:
-                // Should never happen.
-                throw new IllegalStateException(
-                        "The type of TensorImage, " + image.getDataType() + ", is unsupported.");
+        break;
+      case FLOAT32:
+        // Create float array and use one of the 3 identical channels
+        float[] floatArr = new float[w * h * 1];
+        for (int i = 0, j = 0; i < intValues.length; i++) {
+          floatArr[j++] = (float) (intValues[i] & 0xff);
         }
 
-        return image;
+        TensorBuffer bufferFloat = TensorBuffer.createFixedSize(shape, DataType.FLOAT32);
+        bufferFloat.loadArray(floatArr, shape);
+
+        image.load(bufferFloat, GRAYSCALE);
+
+        break;
+      default:
+        // Should never happen.
+        throw new IllegalStateException(
+                "The type of TensorImage, " + image.getDataType() + ", is unsupported.");
     }
 
-    @Override
-    public int getOutputImageHeight(int inputImageHeight, int inputImageWidth) {
-        return inputImageHeight;
-    }
+    return image;
+  }
 
-    @Override
-    public int getOutputImageWidth(int inputImageHeight, int inputImageWidth) {
-        return inputImageWidth;
-    }
+  @Override
+  public int getOutputImageHeight(int inputImageHeight, int inputImageWidth) {
+    return inputImageHeight;
+  }
 
-    @Override
-    public PointF inverseTransform(PointF point, int inputImageHeight, int inputImageWidth) {
-        return point;
-    }
+  @Override
+  public int getOutputImageWidth(int inputImageHeight, int inputImageWidth) {
+    return inputImageWidth;
+  }
+
+  @Override
+  public PointF inverseTransform(PointF point, int inputImageHeight, int inputImageWidth) {
+    return point;
+  }
 }
