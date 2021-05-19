4d39d52196893c2ec5acd0bdc23c32f9026b94c6
Tiezhen WANG <wangtz@google.com>
2021-05-18 21:14:10 -0700

    Audio Task Lib: Add desktop demo for audio classification (C++)
    Example output:
    Head[0]: yamnet_classification
    	category[Speech]: 0.99129
    	category[Inside, small room]: 0.04351
    	category[Conversation]: 0.00642
    	category[Narration, monologue]: 0.00388
    	category[Speech synthesizer]: 0.00198
    	category[Inside, large room or hall]: 0.00176
    Head[1]: bird_classification
    	category[Chestnut-crowned Antpitta]: 0.96322
    	category[Azara's Spinetail]: 0.02851
    	category[White-breasted Wood-Wren]: 0.00815
    
    PiperOrigin-RevId: 374566403

---
+++ tensorflow_lite_support/examples/task/audio/desktop/README.md
@@ -0,0 +1,4 @@
+# CLI Demos for C++ Audio Task APIs
+
+This folder contains simple command-line tools for easily trying out the C++
+Audio Task APIs.
---
+++ tensorflow_lite_support/examples/task/audio/desktop/audio_classifier_lib.cc
@@ -0,0 +1,103 @@
+/* Copyright 2021 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "tensorflow_lite_support/examples/task/audio/desktop/audio_classifier_lib.h"
+
+#include <string>
+#include <vector>
+
+#include "absl/status/status.h"
+#include "absl/strings/str_format.h"
+#include "tensorflow_lite_support/cc/port/status_macros.h"
+#include "tensorflow_lite_support/cc/port/statusor.h"
+#include "tensorflow_lite_support/cc/task/audio/audio_classifier.h"
+#include "tensorflow_lite_support/cc/task/audio/core/audio_buffer.h"
+#include "tensorflow_lite_support/cc/task/audio/proto/classifications_proto_inc.h"
+#include "tensorflow_lite_support/cc/task/core/category.h"
+#include "tensorflow_lite_support/examples/task/audio/desktop/wav/wav_io.h"
+
+namespace tflite {
+namespace task {
+namespace audio {
+
+tflite::support::StatusOr<AudioBuffer> LoadAudioBufferFromFile(
+    const std::string& wav_file, int buffer_size,
+    std::vector<float>* wav_data) {
+  std::string contents = ReadFile(wav_file);
+
+  uint32 decoded_sample_count;
+  uint16 decoded_channel_count;
+  uint32 decoded_sample_rate;
+  RETURN_IF_ERROR(DecodeLin16WaveAsFloatVector(
+      contents, wav_data, &decoded_sample_count, &decoded_channel_count,
+      &decoded_sample_rate));
+
+  if (decoded_sample_count > buffer_size) {
+    decoded_sample_count = buffer_size;
+  }
+
+  return AudioBuffer(
+      wav_data->data(), decoded_sample_count,
+      {decoded_channel_count, static_cast<int>(decoded_sample_rate)});
+}
+
+tflite::support::StatusOr<ClassificationResult> Classify(
+    const std::string& model_path, const std::string& wav_file,
+    bool use_coral) {
+  AudioClassifierOptions options;
+  options.mutable_base_options()->mutable_model_file()->set_file_name(
+      model_path);
+  if (use_coral) {
+    options.mutable_base_options()
+        ->mutable_compute_settings()
+        ->mutable_tflite_settings()
+        ->set_delegate(::tflite::proto::Delegate::EDGETPU_CORAL);
+  }
+  ASSIGN_OR_RETURN(std::unique_ptr<AudioClassifier> classifier,
+                   AudioClassifier::CreateFromOptions(options));
+
+  // `wav_data` holds data loaded from the file and needs to outlive `buffer`.
+  std::vector<float> wav_data;
+  ASSIGN_OR_RETURN(
+      AudioBuffer buffer,
+      LoadAudioBufferFromFile(
+          wav_file, classifier->GetRequiredInputBufferSize(), &wav_data));
+
+  return classifier->Classify(buffer);
+}
+
+void Display(const ClassificationResult& result, float score_threshold) {
+  std::cout << "Note: Only showing classes with score higher than "
+            << score_threshold << std::endl;
+
+  for (int i = 0; i < result.classifications_size(); i++) {
+    const auto& head = result.classifications(i);
+    std::cout << absl::StrFormat("\nHead[%d]: %s\n", i, head.head_name());
+    for (int j = 0; j < head.classes_size(); j++) {
+      const auto& category = head.classes(j);
+      if (category.score() < score_threshold) continue;
+      std::cout << absl::StrFormat("\tcategory[%s]: %.5f\t",
+                                   category.class_name(), category.score());
+      if (!category.display_name().empty()) {
+        std::cout << category.display_name();
+      }
+      std::cout << std::endl;
+    }
+  }
+}
+
+}  // namespace audio
+}  // namespace task
+}  // namespace tflite
