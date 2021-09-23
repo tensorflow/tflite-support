1593728d8f90534fa7fc2a7ca8cbdfe3d1f339e6
Prianka Liz Kariat <prianka.kariat@ymedialabs.com>
2021-09-23 18:10:35 +0530

    Edited documentation

--- tensorflow_lite_support/c/task/common_error_utils.cc
+++ tensorflow_lite_support/c/task/common_error_utils.cc
@@ -68,6 +68,7 @@ void CreateTfLiteErrorWithStatus(const absl::Status& status,
   // from the value 1(kError) and hence will be correctly initialized if
   // directly cast from the integer code derived from TfLiteSupportStatus stored
   // in payload. TfLiteErrorCode omits kOk = 0 of TfLiteSupportStatus.
+  //
   // TODO(prianka): Investigate if switching between error code cast to
   // TfLiteSupportStatus and assigning appropriate value to TfLiteErrorCode is
   // necessary (If 'TfLiteSupportStatus' or other edge cases).
