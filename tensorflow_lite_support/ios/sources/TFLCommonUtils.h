3767de5a2930751a89f85c39d43379b421b14b50
Prianka Liz Kariat <prianka.kariat@ymedialabs.com>
2021-10-25 15:48:58 +0530

    Including common.h instead of importing vision umbrella header

--- tensorflow_lite_support/ios/sources/TFLCommonUtils.h
+++ tensorflow_lite_support/ios/sources/TFLCommonUtils.h
@@ -13,7 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 #import <Foundation/Foundation.h>
-#import <TensorFlowLiteTaskVisionC/TensorFlowLiteTaskVisionC.h>
+#include "tensorflow_lite_support/c/common.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
