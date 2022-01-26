c5406271aa0a33d82936031728b95a73cea6a660
kinaryml <kinar.ravishankar@ymedialabs.com>
2022-01-26 00:54:50 -0800

    updated processor_options.py's comments to include a more generalized heading

--- tensorflow_lite_support/python/task/processor/processor_options.py
+++ tensorflow_lite_support/python/task/processor/processor_options.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Options to configure Embedding APIs."""
+"""Options to configure Vision APIs."""
 
 import dataclasses
 from typing import Optional, List
