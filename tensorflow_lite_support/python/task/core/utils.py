e2e003cbbedb8029c4b06b1a0c79d69b46a8dd85
kinaryml <42828719+kinaryml@users.noreply.github.com>
2022-02-04 06:01:14 +0530

    Generified comment

--- tensorflow_lite_support/python/task/core/utils.py
+++ tensorflow_lite_support/python/task/core/utils.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Utils for Vision APIs."""
+"""Utils for Task APIs."""
 
 from tensorflow_lite_support.python.task.core.proto import configuration_pb2
 
