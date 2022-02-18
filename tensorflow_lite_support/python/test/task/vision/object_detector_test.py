1139c9696980577bb75697e97320e2ffdfe1498d
kinaryml <kinar.ravishankar@ymedialabs.com>
2022-02-18 15:45:06 -0800

    removed list comprehension

--- tensorflow_lite_support/python/test/task/vision/object_detector_test.py
+++ tensorflow_lite_support/python/test/task/vision/object_detector_test.py
@@ -66,12 +66,12 @@ class ObjectDetectorTest(parameterized.TestCase, base_test.BaseTestCase):
     expected_result = detections_pb2.DetectionResult()
 
     for index in range(len(expected_detections)):
-      bounding_box, categories = expected_detections[index]
+      bounding_box, category = expected_detections[index]
       detection = detections_pb2.Detection()
-      bounding_box = bounding_box_pb2.BoundingBox(**bounding_box)
-      detection.bounding_box.CopyFrom(bounding_box)
-      detection.classes.extend([
-        class_pb2.Category(**args) for args in categories])
+      detection.bounding_box.CopyFrom(
+        bounding_box_pb2.BoundingBox(**bounding_box))
+      detection.classes.append(
+        class_pb2.Category(**category))
       expected_result.detections.append(detection)
 
     expected_result_dict = json.loads(
@@ -83,85 +83,70 @@ class ObjectDetectorTest(parameterized.TestCase, base_test.BaseTestCase):
     (
       {
         'origin_x': 54, 'origin_y': 396, 'width': 393, 'height': 196
-      }, [
-        {
-          'index': 16,
-          'score': 0.64453125,
-          'class_name': 'cat'
-        }
-      ]
+      },
+      {
+        'index': 16,
+        'score': 0.64453125,
+        'class_name': 'cat'
+      }
     ), (
       {
         'origin_x': 602, 'origin_y': 157, 'width': 394, 'height': 447
-      }, [
-        {
-          'index': 16,
-          'score': 0.59765625,
-          'class_name': 'cat'
-        }
-      ]
+      }, {
+        'index': 16,
+        'score': 0.59765625,
+        'class_name': 'cat'
+      }
     ), (
       {
         'origin_x': 261, 'origin_y': 394, 'width': 179, 'height': 209
-      }, [
-        {
-          'index': 16,
-          'score': 0.5625,
-          'class_name': 'cat'
-        }
-      ]
+      }, {
+        'index': 16,
+        'score': 0.5625,
+        'class_name': 'cat'
+      }
     ), (
       {
         'origin_x': 389, 'origin_y': 197, 'width': 276, 'height': 409
-      }, [
-        {
-          'index': 17,
-          'score': 0.51171875,
-          'class_name': 'dog'
-        }
-      ]
+      }, {
+        'index': 17,
+        'score': 0.51171875,
+        'class_name': 'dog'
+      }
     )
   ]), (ModelFileType.FILE_CONTENT, 0.5, [
     (
       {
         'origin_x': 54, 'origin_y': 396, 'width': 393, 'height': 196
-      }, [
-        {
-          'index': 16,
-          'score': 0.64453125,
-          'class_name': 'cat'
-        }
-      ]
+      }, {
+        'index': 16,
+        'score': 0.64453125,
+        'class_name': 'cat'
+      }
     ), (
       {
         'origin_x': 602, 'origin_y': 157, 'width': 394, 'height': 447
-      }, [
-        {
-          'index': 16,
-          'score': 0.59765625,
-          'class_name': 'cat'
-        }
-      ]
+      }, {
+        'index': 16,
+        'score': 0.59765625,
+        'class_name': 'cat'
+      }
     ), (
       {
         'origin_x': 261, 'origin_y': 394, 'width': 179, 'height': 209
-      }, [
-        {
-          'index': 16,
-          'score': 0.5625,
-          'class_name': 'cat'
-        }
-      ]
+      }, {
+        'index': 16,
+        'score': 0.5625,
+        'class_name': 'cat'
+      }
     ), (
       {
         'origin_x': 389, 'origin_y': 197, 'width': 276, 'height': 409
-      }, [
-        {
-          'index': 17,
-          'score': 0.51171875,
-          'class_name': 'dog'
-        }
-      ]
+      }, {
+        'index': 17,
+        'score': 0.51171875,
+        'class_name': 'dog'
+      }
     )
   ]))
   def test_detect_model(self, model_file_type, score_threshold, expected_detections):
