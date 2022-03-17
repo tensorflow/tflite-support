bd0e5bca24591434b21eac8763cc07f6a1d2d585
kinaryml <kinar.ravishankar@ymedialabs.com>
2022-03-17 06:39:09 -0700

    Updated docstrings and comments

--- tensorflow_lite_support/python/task/processor/proto/segmentations_pb2.py
+++ tensorflow_lite_support/python/task/processor/proto/segmentations_pb2.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Embedding result protobuf."""
+"""Segmentations protobuf."""
 
 from tensorflow_lite_support.cc.task.vision.proto import segmentations_pb2
 
