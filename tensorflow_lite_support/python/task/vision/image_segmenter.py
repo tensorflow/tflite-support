9c468e4bfe78c56f1a4937d04a68db542af19ce9
kinaryml <kinar.ravishankar@ymedialabs.com>
2022-03-17 06:36:19 -0700

    Added image segmenter implementation along with a test for category mask

---
+++ tensorflow_lite_support/python/task/vision/image_segmenter.py
@@ -0,0 +1,122 @@
+# Copyright 2022 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Image classifier task."""
+
+import dataclasses
+from typing import Optional
+
+from tensorflow_lite_support.python.task.core import task_options
+from tensorflow_lite_support.python.task.core import task_utils
+from tensorflow_lite_support.python.task.processor.proto import segmentation_options_pb2
+from tensorflow_lite_support.python.task.processor.proto import segmentations_pb2
+from tensorflow_lite_support.python.task.vision.core import tensor_image
+from tensorflow_lite_support.python.task.vision.core.pybinds import image_utils
+from tensorflow_lite_support.python.task.vision.pybinds import _pywrap_image_segmenter
+from tensorflow_lite_support.python.task.vision.pybinds import image_segmenter_options_pb2
+
+_ProtoImageSegmenterOptions = image_segmenter_options_pb2.ImageSegmenterOptions
+_CppImageSegmenter = _pywrap_image_segmenter.ImageSegmenter
+_BaseOptions = task_options.BaseOptions
+_ExternalFile = task_options.ExternalFile
+
+
+@dataclasses.dataclass
+class ImageSegmenterOptions:
+  """Options for the image segmenter task."""
+  base_options: _BaseOptions
+  segmentation_options: Optional[
+      segmentation_options_pb2.SegmentationOptions] = None
+
+
+class ImageSegmenter(object):
+  """Class that performs segmentation on images."""
+
+  def __init__(self, options: ImageSegmenterOptions,
+               segmenter: _CppImageSegmenter) -> None:
+    """Initializes the `ImageSegmenter` object."""
+    # Creates the object of C++ ImageSegmenter class.
+    self._options = options
+    self._segmenter = segmenter
+
+  @classmethod
+  def create_from_file(cls, file_path: str) -> "ImageSegmenter":
+    """Creates the `ImageSegmenter` object from a TensorFlow Lite model.
+
+    Args:
+      file_path: Path to the model.
+    Returns:
+      `ImageSegmenter` object that's created from `options`.
+    Raises:
+      status.StatusNotOk if failed to create `ImageSegmenter` object from the
+      provided file such as invalid file.
+    """
+    # TODO(b/220931229): Raise RuntimeError instead of status.StatusNotOk.
+    # Need to import the module to catch this error:
+    # `from pybind11_abseil import status`
+    # see https://github.com/pybind/pybind11_abseil#abslstatusor.
+    base_options = _BaseOptions(
+        model_file=_ExternalFile(file_name=file_path))
+    options = ImageSegmenterOptions(base_options=base_options)
+    return cls.create_from_options(options)
+
+  @classmethod
+  def create_from_options(cls,
+                          options: ImageSegmenterOptions) -> "ImageSegmenter":
+    """Creates the `ImageSegmenter` object from image segmenter options.
+
+    Args:
+      options: Options for the image segmenter task.
+    Returns:
+      `ImageSegmenter` object that's created from `options`.
+    Raises:
+      status.StatusNotOk if failed to create `ImageSegmenter` object from
+      `ImageSegmenterOptionsn` such as missing the model.
+    """
+    # TODO(b/220931229): Raise RuntimeError instead of status.StatusNotOk.
+    # Need to import the module to catch this error:
+    # `from pybind11_abseil import status`
+    # see https://github.com/pybind/pybind11_abseil#abslstatusor.
+    proto_options = _ProtoImageSegmenterOptions()
+    proto_options.base_options.CopyFrom(
+        task_utils.ConvertToProtoBaseOptions(options.base_options))
+
+    # Updates values from classification_options.
+    if options.segmentation_options:
+      if options.segmentation_options.display_names_locale:
+        proto_options.display_names_locale = options.segmentation_options.display_names_locale
+      if options.segmentation_options.output_type:
+        proto_options.output_type = options.segmentation_options.output_type
+
+    segmenter = _CppImageSegmenter.create_from_options(proto_options)
+
+    return cls(options, segmenter)
+
+  def segment(
+      self,
+      image: tensor_image.TensorImage
+  ) -> segmentations_pb2.SegmentationResult:
+    """Performs segmentation on the provided TensorImage.
+
+    Args:
+      image: Tensor image, used to extract the feature vectors.
+    Returns:
+      segmentation result.
+    Raises:
+      status.StatusNotOk if failed to get the feature vector. Need to import the
+        module to catch this error: `from pybind11_abseil
+        import status`, see
+        https://github.com/pybind/pybind11_abseil#abslstatusor.
+    """
+    image_data = image_utils.ImageData(image.get_buffer())
+    return self._segmenter.segment(image_data)
---
+++ tensorflow_lite_support/python/test/task/vision/image_segmenter_test.py
@@ -0,0 +1,197 @@
+# Copyright 2022 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for image_classifier."""
+
+import enum
+import json
+
+import numpy as np
+from absl.testing import parameterized
+from google.protobuf import json_format
+# TODO(b/220067158): Change to import tensorflow and leverage tf.test once
+# fixed the dependency issue.
+import unittest
+from tensorflow_lite_support.python.task.core import task_options
+from tensorflow_lite_support.python.task.processor.proto import segmentation_options_pb2
+from tensorflow_lite_support.python.task.processor.proto import segmentations_pb2
+from tensorflow_lite_support.python.task.vision import image_segmenter
+from tensorflow_lite_support.python.task.vision.core import tensor_image
+from tensorflow_lite_support.python.test import base_test
+from tensorflow_lite_support.python.test import test_util
+
+OutputType = segmentation_options_pb2.OutputType
+
+_BaseOptions = task_options.BaseOptions
+_ExternalFile = task_options.ExternalFile
+_ImageSegmenter = image_segmenter.ImageSegmenter
+_ImageSegmenterOptions = image_segmenter.ImageSegmenterOptions
+
+_MODEL_FILE = 'deeplabv3.tflite'
+_IMAGE_FILE = 'segmentation_input_image.jpg'
+_SEGMENTATION_FILE = 'segmentation_ground_truth_image.png'
+_EXPECTED_COLORED_LABELS = [
+  {'r': 0, 'g': 0, 'b': 0, 'className': 'background'},
+  {'r': 128, 'g': 0, 'b': 0, 'className': 'aeroplane'},
+  {'r': 0, 'g': 128, 'b': 0, 'className': 'bicycle'},
+  {'r': 128, 'g': 128, 'b': 0, 'className': 'bird'},
+  {'r': 0, 'g': 0, 'b': 128, 'className': 'boat'},
+  {'r': 128, 'g': 0, 'b': 128, 'className': 'bottle'},
+  {'r': 0, 'g': 128, 'b': 128, 'className': 'bus'},
+  {'r': 128, 'g': 128, 'b': 128, 'className': 'car'},
+  {'r': 64, 'g': 0, 'b': 0, 'className': 'cat'},
+  {'r': 192, 'g': 0, 'b': 0, 'className': 'chair'},
+  {'r': 64, 'g': 128, 'b': 0, 'className': 'cow'},
+  {'r': 192, 'g': 128, 'b': 0, 'className': 'dining table'},
+  {'r': 64, 'g': 0, 'b': 128, 'className': 'dog'},
+  {'r': 192, 'g': 0, 'b': 128, 'className': 'horse'},
+  {'r': 64, 'g': 128, 'b': 128, 'className': 'motorbike'},
+  {'r': 192, 'g': 128, 'b': 128, 'className': 'person'},
+  {'r': 0, 'g': 64, 'b': 0, 'className': 'potted plant'},
+  {'r': 128, 'g': 64, 'b': 0, 'className': 'sheep'},
+  {'r': 0, 'g': 192, 'b': 0, 'className': 'sofa'},
+  {'r': 128, 'g': 192, 'b': 0, 'className': 'train'},
+  {'r': 0, 'g': 64, 'b': 128, 'className': 'tv'}
+]
+_EXPECTED_LABELS = ['background', 'person', 'horse']
+_MATCH_PIXELS_THRESHOLD = 0.01
+
+
+class ModelFileType(enum.Enum):
+  FILE_CONTENT = 1
+  FILE_NAME = 2
+
+
+class ImageSegmenterTest(parameterized.TestCase, base_test.BaseTestCase):
+
+  def setUp(self):
+    super().setUp()
+    self.test_image_path = test_util.get_test_data_path(_IMAGE_FILE)
+    self.test_seg_path = test_util.get_test_data_path(_SEGMENTATION_FILE)
+    self.model_path = test_util.get_test_data_path(_MODEL_FILE)
+
+  @staticmethod
+  def create_segmenter_from_options(model_file, **segmentation_options):
+    base_options = _BaseOptions(model_file=model_file)
+    segmentation_options = segmentation_options_pb2.SegmentationOptions(
+        **segmentation_options)
+    options = _ImageSegmenterOptions(
+        base_options=base_options,
+        segmentation_options=segmentation_options)
+    segmenter = _ImageSegmenter.create_from_options(options)
+    return segmenter
+
+  @staticmethod
+  def segmentation_map_to_image(segmentation: segmentations_pb2.Segmentation):
+    """Convert the segmentation result into a RGB image.
+      Params:
+        segmentation: An output of a image segmentation model.
+      Returns:
+        seg_map_img: The visualized segmentation result as an RGB image.
+        found_colored_labels: The list of ColoredLabels found in the image.
+    """
+    # Get the category mask array.
+    masks = np.array(bytearray(segmentation.category_mask))
+    colored_labels = segmentation.colored_labels
+
+    # Get the list of unique labels from the model output.
+    found_label_indices, inverse_map, counts = np.unique(
+      masks, return_inverse=True, return_counts=True)
+    count_dict = dict(zip(found_label_indices, counts))
+
+    # Sort the list of unique label so that the class with the most pixel will
+    # come first.
+    sorted_label_indices = sorted(
+      found_label_indices, key=lambda index: count_dict[index], reverse=True)
+    found_colored_labels = [
+      colored_labels[idx].class_name for idx in sorted_label_indices
+    ]
+
+    # Convert segmentation map into RGB image of the same size as the input
+    # image. Note: We use the inverse map to avoid running the heavy loop in
+    # Python and pass it over to Numpy's C++ implementation to improve
+    # performance.
+    found_colors = [
+      # BGR mode.
+      (colored_labels[idx].b, colored_labels[idx].g, colored_labels[idx].r)
+      for idx in found_label_indices
+    ]
+    output_shape = [segmentation.width, segmentation.height, 3]
+    seg_map_img = np.array(found_colors)[inverse_map] \
+                    .reshape(output_shape) \
+                    .astype(np.uint8)
+
+    return seg_map_img, found_colored_labels
+
+  @parameterized.parameters(
+    (ModelFileType.FILE_NAME, OutputType.CATEGORY_MASK,
+     _EXPECTED_COLORED_LABELS, _EXPECTED_LABELS),
+    (ModelFileType.FILE_CONTENT, OutputType.CATEGORY_MASK,
+     _EXPECTED_COLORED_LABELS, _EXPECTED_LABELS))
+  def test_segmentation_category_mask(self, model_file_type, output_type,
+                                      expected_colored_labels, expected_labels):
+    """Check if category mask match with ground truth."""
+    # Creates segmenter.
+    if model_file_type is ModelFileType.FILE_NAME:
+      model_file = _ExternalFile(file_name=self.model_path)
+    elif model_file_type is ModelFileType.FILE_CONTENT:
+      with open(self.model_path, 'rb') as f:
+        model_content = f.read()
+      model_file = _ExternalFile(file_content=model_content)
+    else:
+      # Should never happen
+      raise ValueError('model_file_type is invalid.')
+
+    segmenter = self.create_segmenter_from_options(model_file,
+                                                   output_type=output_type)
+
+    # Loads image.
+    image = tensor_image.TensorImage.from_file(self.test_image_path)
+
+    # Performs image segmentation on the input.
+    image_result = segmenter.segment(image)
+    segmentation = image_result.segmentation[0]
+    segmentation_colored_labels = json.loads(
+      json_format.MessageToJson(segmentation))['coloredLabels']
+
+    # Comparing results.
+    self.assertDeepAlmostEqual(
+      segmentation_colored_labels, expected_colored_labels)
+
+    if output_type != segmentation_options_pb2.OutputType.CATEGORY_MASK:
+      raise ValueError(
+        'Unsupported output type. Only OutputType.CATEGORY_MASK is supported.')
+
+    seg_map_img, found_labels = self.segmentation_map_to_image(segmentation)
+    result_pixels = seg_map_img.flatten()
+
+    # Loads ground truth segmentation file.
+    gt_segmentation = tensor_image.TensorImage.from_file(self.test_seg_path)
+    ground_truth_pixels = gt_segmentation.get_buffer().flatten()
+
+    self.assertEqual(
+      len(result_pixels), len(ground_truth_pixels),
+      "Segmentation mask must have the same size as ground truth.")
+
+    inconsistent_pixels = [1 for idx in range(len(result_pixels))
+                           if result_pixels[idx] != ground_truth_pixels[idx]]
+
+    self.assertLessEqual(
+      len(inconsistent_pixels) / len(result_pixels), _MATCH_PIXELS_THRESHOLD,
+      "Segmentation mask value must be the same size as ground truth.")
+
+    self.assertEqual(found_labels, expected_labels, "Labels do not match.")
+
+
+if __name__ == '__main__':
+  unittest.main()
