ccfe2d30afbff3ac13196f499c7313f340340a53
Prianka Liz Kariat <prianka.kariat@ymedialabs.com>
2022-04-13 17:25:53 +0530

    Added copying for arrays

--- tensorflow_lite_support/ios/task/processor/sources/TFLClassificationResult.m
+++ tensorflow_lite_support/ios/task/processor/sources/TFLClassificationResult.m
@@ -21,7 +21,7 @@ limitations under the License.
   self = [super init];
   if (self) {
     _headIndex = headIndex;
-    _categories = categories;
+    _categories = [categories copy];
   }
   return self;
 }
@@ -35,7 +35,7 @@ limitations under the License.
 - (instancetype)initWithClassifications:(NSArray<TFLClassifications *> *)classifications {
   self = [super init];
   if (self) {
-    _classifications = classifications;
+    _classifications = [classifications copy];
   }
   return self;
 }
