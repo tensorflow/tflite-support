b063694315dbc39bd454dd5625a49fd36eec36ad
Prianka Liz Kariat <prianka.kariat@ymedialabs.com>
2022-04-13 18:23:53 +0530

    Removed copy from readonly properties

--- tensorflow_lite_support/ios/task/processor/sources/TFLDetectionResult.h
+++ tensorflow_lite_support/ios/task/processor/sources/TFLDetectionResult.h
@@ -30,7 +30,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 /** The array of predicted classes, usually sorted by descending scores (e.g.from high to low
  * probability). */
-@property(nonatomic, copy, readonly) NSArray<TFLCategory *> *categories;
+@property(nonatomic, readonly) NSArray<TFLCategory *> *categories;
 
 /**
  * Initializes TFLDetection.
@@ -49,7 +49,7 @@ NS_ASSUME_NONNULL_BEGIN
 /** Encapsulates results of any object detection task. */
 @interface TFLDetectionResult : NSObject
 
-@property(nonatomic, copy, readonly) NSArray<TFLDetection *> *detections;
+@property(nonatomic, readonly) NSArray<TFLDetection *> *detections;
 
 /**
  * Initializes TFLDetectionResult.
