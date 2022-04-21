ad4edfdc34679714ae2977881cb1a908bc1245ed
Prianka Liz Kariat <prianka.kariat@ymedialabs.com>
2022-04-21 09:16:17 +0530

    Removed unused variables

--- tensorflow_lite_support/ios/task/vision/utils/sources/GMLImage+Utils.m
+++ tensorflow_lite_support/ios/task/vision/utils/sources/GMLImage+Utils.m
@@ -285,7 +285,6 @@
   if (ciImage.pixelBuffer) {
     width = (int)CVPixelBufferGetWidth(ciImage.pixelBuffer);
     height = (int)CVPixelBufferGetHeight(ciImage.pixelBuffer);
-    NSInteger channelCount = 4;
 
     buffer = [TFLCVPixelBufferUtils createRGBImageDatafromCVPixelBuffer:ciImage.pixelBuffer
                                                                   error:error];
