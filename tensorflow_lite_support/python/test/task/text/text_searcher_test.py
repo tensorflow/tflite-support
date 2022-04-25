c2c303a920d9f1d19b856c7d21ea0190c418a6b2
kinaryml <kinar.ravishankar@ymedialabs.com>
2022-04-25 13:03:06 -0700

    Added more tests for text searcher

--- tensorflow_lite_support/python/test/task/text/text_searcher_test.py
+++ tensorflow_lite_support/python/test/task/text/text_searcher_test.py
@@ -55,23 +55,137 @@ nearest_neighbors {
 }
 """
 
+_BERT_MODEL = 'mobilebert_embedding_with_metadata.tflite'
+_BERT_INDEX = 'mobilebert_index.ldb'
+_EXPECTED_BERT_SEARCH_PARAMS = """
+nearest_neighbors {
+  metadata: "The weather was excellent."
+  distance: 0.0
+}
+nearest_neighbors {
+  metadata: "It was a sunny day."
+  distance: 0.115369
+}
+nearest_neighbors {
+  metadata: "The sun was shining on that day."
+  distance: 0.230017
+}
+nearest_neighbors {
+  metadata: "He was very happy with his newly bought car."
+  distance: 0.324563
+}
+nearest_neighbors {
+  metadata: "The cat is chasing after the mouse."
+  distance: 0.966928
+}
+"""
+
+_MAX_RESULTS = 2
+
 
 class ModelFileType(enum.Enum):
   FILE_CONTENT = 1
   FILE_NAME = 2
 
 
+class IndexFileType(enum.Enum):
+  FILE_CONTENT = 1
+  FILE_NAME = 2
+
+
 class TextSearcherTest(parameterized.TestCase, tf.test.TestCase):
+
+  def setUp(self):
+    super().setUp()
+    self.model_path = test_util.get_test_data_path(_REGEX_MODEL)
+    self.index_path = test_util.get_test_data_path(_REGEX_INDEX)
+
+  def test_create_from_file_succeeds_with_valid_model_and_index_paths(self):
+    # Creates with default option and valid model and index files successfully.
+    searcher = _TextSearcher.create_from_file(
+      self.model_path, self.index_path)
+    self.assertIsInstance(searcher, _TextSearcher)
+
+  def test_create_from_options_succeeds_with_valid_model_and_index_paths(self):
+    options = _TextSearcherOptions(
+      base_options=_BaseOptions(file_name=self.model_path),
+      search_options=_SearchOptions(index_file_name=self.index_path))
+    searcher = _TextSearcher.create_from_options(options)
+    self.assertIsInstance(searcher, _TextSearcher)
+
+  def test_create_from_options_succeeds_with_valid_model_content(self):
+    # Creates with options containing model content successfully.
+    with open(self.model_path, 'rb') as f:
+      options = _TextSearcherOptions(
+        base_options=_BaseOptions(file_content=f.read()),
+        search_options=_SearchOptions(index_file_name=self.index_path))
+      searcher = _TextSearcher.create_from_options(options)
+      self.assertIsInstance(searcher, _TextSearcher)
+
+  def test_create_from_options_fails_with_invalid_index_path(self):
+    # Invalid index path.
+    with self.assertRaisesRegex(
+        ValueError,
+        r'Missing mandatory `index_file` field in `search_options`'):
+      options = _TextSearcherOptions(
+        base_options=_BaseOptions(file_name=self.model_path))
+      _TextSearcher.create_from_options(options)
+
+  def test_create_from_options_fails_with_invalid_model_path(self):
+    # Invalid empty model path.
+    with self.assertRaisesRegex(
+        ValueError,
+        r"ExternalFile must specify at least one of 'file_content', "
+        r"'file_name' or 'file_descriptor_meta'."):
+      options = _TextSearcherOptions(
+        base_options=_BaseOptions(file_name=""),
+        search_options=_SearchOptions(index_file_name=self.index_path))
+      _TextSearcher.create_from_options(options)
+
+  def test_create_from_options_fails_with_invalid_quantization(self):
+    # Invalid quantization option.
+    with self.assertRaisesRegex(
+        ValueError,
+        r'Setting EmbeddingOptions.normalize = true is not allowed in '
+        r'searchers.'):
+      options = _TextSearcherOptions(
+        base_options=_BaseOptions(file_name=self.model_path),
+        embedding_options=_EmbeddingOptions(quantize=True),
+        search_options=_SearchOptions(index_file_name=self.index_path))
+      _TextSearcher.create_from_options(options)
+
+  def test_create_from_options_fails_with_invalid_num_results(self):
+    # Invalid num results option.
+    with self.assertRaisesRegex(
+        ValueError,
+        r'SearchOptions.max_results must be > 0, found -1.'):
+      options = _TextSearcherOptions(
+        base_options=_BaseOptions(file_name=self.model_path),
+        search_options=_SearchOptions(index_file_name=self.index_path,
+                                      max_results=-1))
+      _TextSearcher.create_from_options(options)
 
   @parameterized.parameters(
       (_REGEX_MODEL, _REGEX_INDEX, True, False, ModelFileType.FILE_NAME,
-       _EXPECTED_REGEX_SEARCH_PARAMS),
+       IndexFileType.FILE_NAME, _EXPECTED_REGEX_SEARCH_PARAMS),
       (_REGEX_MODEL, _REGEX_INDEX, True, False, ModelFileType.FILE_CONTENT,
-       _EXPECTED_REGEX_SEARCH_PARAMS),
+       IndexFileType.FILE_NAME, _EXPECTED_REGEX_SEARCH_PARAMS),
+      (_REGEX_MODEL, _REGEX_INDEX, True, False, ModelFileType.FILE_NAME,
+       IndexFileType.FILE_CONTENT, _EXPECTED_REGEX_SEARCH_PARAMS),
+      (_REGEX_MODEL, _REGEX_INDEX, True, False, ModelFileType.FILE_CONTENT,
+       IndexFileType.FILE_CONTENT, _EXPECTED_REGEX_SEARCH_PARAMS),
+      (_BERT_MODEL, _BERT_INDEX, True, False, ModelFileType.FILE_NAME,
+       IndexFileType.FILE_NAME, _EXPECTED_BERT_SEARCH_PARAMS),
+      (_BERT_MODEL, _BERT_INDEX, True, False, ModelFileType.FILE_CONTENT,
+       IndexFileType.FILE_NAME, _EXPECTED_BERT_SEARCH_PARAMS),
+      (_BERT_MODEL, _BERT_INDEX, True, False, ModelFileType.FILE_NAME,
+       IndexFileType.FILE_CONTENT, _EXPECTED_BERT_SEARCH_PARAMS),
+      (_BERT_MODEL, _BERT_INDEX, True, False, ModelFileType.FILE_CONTENT,
+       IndexFileType.FILE_CONTENT, _EXPECTED_BERT_SEARCH_PARAMS)
   )
   def test_search(self, model_name, index_name, l2_normalize, quantize,
-                  model_file_type, expected_result_text_proto):
-    # Create searcher.
+                  model_file_type, index_file_type, expected_result_text_proto):
+    # Create BaseOptions.
     model_path = test_util.get_test_data_path(model_name)
     if model_file_type is ModelFileType.FILE_NAME:
       base_options = _BaseOptions(file_name=model_path)
@@ -83,11 +197,23 @@ class TextSearcherTest(parameterized.TestCase, tf.test.TestCase):
       # Should never happen
       raise ValueError('model_file_type is invalid.')
 
-    index_file_path = test_util.get_test_data_path(index_name)
+    # Create SearchOptions.
+    index_path = test_util.get_test_data_path(index_name)
+    if index_file_type is IndexFileType.FILE_NAME:
+      search_options = _SearchOptions(index_file_name=index_path)
+    elif index_file_type is IndexFileType.FILE_CONTENT:
+      with open(index_path, 'rb') as f:
+        index_content = f.read()
+      search_options = _SearchOptions(index_file_content=index_content)
+    else:
+      # Should never happen
+      raise ValueError('index_file_type is invalid.')
+
+    # Create searcher.
     options = _TextSearcherOptions(
         base_options,
         _EmbeddingOptions(l2_normalize=l2_normalize, quantize=quantize),
-        _SearchOptions(index_file_name=index_file_path))
+        search_options)
     searcher = _TextSearcher.create_from_options(options)
 
     # Perform text search.
@@ -98,6 +224,22 @@ class TextSearcherTest(parameterized.TestCase, tf.test.TestCase):
 
     # Get user info and compare values.
     self.assertEqual(searcher.get_user_info(), 'userinfo')
+
+  def test_max_results_option(self):
+    # Create searcher.
+    base_options = _BaseOptions(file_name=self.model_path)
+    search_options = _SearchOptions(index_file_name=self.index_path,
+                                    max_results=_MAX_RESULTS)
+    options = _TextSearcherOptions(
+        base_options, _EmbeddingOptions(l2_normalize=True), search_options)
+    searcher = _TextSearcher.create_from_options(options)
+
+    # Perform text search.
+    text_search_result = searcher.search('The weather was excellent.')
+    nearest_neighbors = text_search_result.nearest_neighbors
+
+    self.assertLessEqual(
+      len(nearest_neighbors), _MAX_RESULTS, 'Too many results returned.')
 
 
 if __name__ == '__main__':
