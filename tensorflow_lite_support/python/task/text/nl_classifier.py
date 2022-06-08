9805f4ff4ea3a6f93da84c83fd73121b2601c880
kinaryml <kinar.ravishankar@ymedialabs.com>
2022-06-07 21:33:35 -0700

    Removed NlClassificationOptions from NLClassifierOptions and added ClassificationOptions to filter results

--- tensorflow_lite_support/python/task/text/nl_classifier.py
+++ tensorflow_lite_support/python/task/text/nl_classifier.py
@@ -17,11 +17,13 @@ import dataclasses
 
 from tensorflow_lite_support.python.task.core import base_options as base_options_module
 from tensorflow_lite_support.python.task.processor.proto import classifications_pb2
+from tensorflow_lite_support.python.task.processor.proto import classification_options_pb2
 from tensorflow_lite_support.python.task.processor.proto import nl_classification_options_pb2
 from tensorflow_lite_support.python.task.text.pybinds import _pywrap_nl_classifier
 
 _CppNLClassifier = _pywrap_nl_classifier.NLClassifier
 _BaseOptions = base_options_module.BaseOptions
+_ClassificationOptions = classification_options_pb2.ClassificationOptions
 _NLClassificationOptions = nl_classification_options_pb2.NLClassificationOptions
 
 
@@ -29,7 +31,7 @@ _NLClassificationOptions = nl_classification_options_pb2.NLClassificationOptions
 class NLClassifierOptions:
   """Options for the NL classifier task."""
   base_options: _BaseOptions
-  nl_classification_options: _NLClassificationOptions = _NLClassificationOptions()
+  classification_options: _ClassificationOptions = _ClassificationOptions()
 
 
 class NLClassifier(object):
@@ -71,12 +73,25 @@ class NLClassifier(object):
       `NLClassifier` object that's created from `options`.
     Raises:
       ValueError: If failed to create `NLClassifier` object from
-        `NLClassifierOptions` such as missing the model.
+        `NLClassifierOptions` such as missing the model or if any of the
+        classification options is invalid.
       RuntimeError: If other types of error occurred.
     """
+    classification_options = options.classification_options
+
+    if classification_options.max_results == 0:
+      raise ValueError("Invalid `max_results` option: value must be != 0")
+
+    if classification_options.category_name_allowlist is not None and \
+        classification_options.category_name_denylist is not None:
+      if len(classification_options.category_name_allowlist) > 0 and \
+          len(classification_options.category_name_denylist) > 0:
+        raise ValueError(
+          "`class_name_allowlist` and `class_name_denylist` are mutually "
+          "exclusive options.")
+
     classifier = _CppNLClassifier.create_from_options(
-        options.base_options.to_pb2(),
-        options.nl_classification_options.to_pb2())
+        options.base_options.to_pb2(), _NLClassificationOptions())
     return cls(options, classifier)
 
   def classify(self, text: str) -> classifications_pb2.ClassificationResult:
@@ -93,8 +108,66 @@ class NLClassifier(object):
       RuntimeError: If failed to calculate the embedding vector.
     """
     classification_result = self._classifier.classify(text)
-    return classifications_pb2.ClassificationResult.create_from_pb2(
-      classification_result)
+    classification_result = classifications_pb2.ClassificationResult.\
+        create_from_pb2(classification_result)
+    return self._postprocess(classification_result)
+
+  def _postprocess(self, result: classifications_pb2.ClassificationResult):
+    """Post-process the classification output based on classification options.
+
+    Args:
+      result: the raw classification result.
+
+    Returns:
+      The filtered classification result.
+
+    Raises:
+      ValueError: If any of the classification options is invalid.
+    """
+    classification_options = self.options.classification_options
+
+    if classification_options.category_name_allowlist is not None and \
+        classification_options.category_name_denylist is not None:
+      if len(classification_options.category_name_allowlist) == 0 and \
+          len(classification_options.category_name_denylist) == 0:
+        return result
+
+    # Sort in descending order (higher score is better).
+    categories = result.classifications[0].categories
+    categories = sorted(
+        categories, key=lambda category: category.score, reverse=True)
+
+    # Filter out classification in deny list
+    filtered_results = categories
+    if classification_options.category_name_denylist is not None:
+      filtered_results = list(
+        filter(
+          lambda category: category.category_name not in classification_options.
+            category_name_denylist, filtered_results))
+
+    # Keep only classification in allow list
+    if classification_options.category_name_allowlist is not None:
+      filtered_results = list(
+        filter(
+          lambda category: category.category_name in classification_options.
+            category_name_allowlist, filtered_results))
+
+    # Filter out classification in score threshold
+    if classification_options.score_threshold is not None:
+      filtered_results = list(
+        filter(
+          lambda category: category.score >= classification_options.
+            score_threshold, filtered_results))
+
+    # Only return maximum of max_results classification.
+    if classification_options.max_results is not None:
+      if classification_options.max_results > 0:
+        result_count = min(len(filtered_results),
+                           classification_options.max_results)
+        filtered_results = filtered_results[:result_count]
+
+    result.classifications[0].categories = filtered_results
+    return result
 
   @property
   def options(self) -> NLClassifierOptions:
--- tensorflow_lite_support/python/test/task/text/nl_classifier_test.py
+++ tensorflow_lite_support/python/test/task/text/nl_classifier_test.py
@@ -21,6 +21,7 @@ import tensorflow as tf
 from tensorflow_lite_support.python.task.core import base_options as base_options_module
 from tensorflow_lite_support.python.task.processor.proto import class_pb2
 from tensorflow_lite_support.python.task.processor.proto import classifications_pb2
+from tensorflow_lite_support.python.task.processor.proto import classification_options_pb2
 from tensorflow_lite_support.python.task.processor.proto import nl_classification_options_pb2
 from tensorflow_lite_support.python.task.text import nl_classifier
 from tensorflow_lite_support.python.test import test_util
@@ -31,6 +32,7 @@ _Category = class_pb2.Category
 _Classifications = classifications_pb2.Classifications
 _ClassificationResult = classifications_pb2.ClassificationResult
 _NLClassifierOptions = nl_classifier.NLClassifierOptions
+_ClassificationOptions = classification_options_pb2.ClassificationOptions
 _NLClassificationOptions = nl_classification_options_pb2.NLClassificationOptions
 
 
@@ -43,19 +45,34 @@ _EXPECTED_RESULTS_OF_POSITIVE_INPUT = _ClassificationResult(
       categories=[
         _Category(
           index=0,
-          score=0.486573,
-          display_name='',
-          category_name='Negative'),
-        _Category(
-          index=0,
           score=0.513427,
           display_name='',
           category_name='Positive'
-        )
+        ),
+        _Category(
+          index=0,
+          score=0.486573,
+          display_name='',
+          category_name='Negative'),
       ],
       head_index=0,
       head_name=''
     )])
+
+_INPUT_STR = "hello"
+_ALLOW_LIST = ['Positive', 'Negative']
+_DENY_LIST = ['Negative']
+_SCORE_THRESHOLD = 0.5
+_MAX_RESULTS = 1
+
+
+def _create_classifier_from_options(base_options, **classification_options):
+  classification_options = classification_options_pb2.ClassificationOptions(
+    **classification_options)
+  options = _NLClassifierOptions(
+    base_options=base_options, classification_options=classification_options)
+  classifier = _NLClassifier.create_from_options(options)
+  return classifier
 
 
 class ModelFileType(enum.Enum):
@@ -64,6 +81,10 @@ class ModelFileType(enum.Enum):
 
 
 class NLClassifierTest(parameterized.TestCase, tf.test.TestCase):
+
+  def setUp(self):
+    super().setUp()
+    self.model_path = test_util.get_test_data_path(_REGEX_TOKENIZER_MODEL)
 
   @parameterized.parameters(
       # Regex tokenizer model.
@@ -91,6 +112,82 @@ class NLClassifierTest(parameterized.TestCase, tf.test.TestCase):
     text_classification_result = classifier.classify(text)
     self.assertProtoEquals(text_classification_result.to_pb2(),
                            expected_classification_result.to_pb2())
+
+  def test_max_results_option(self):
+    # Creates classifier.
+    base_options = _BaseOptions(file_name=self.model_path)
+
+    classifier = _create_classifier_from_options(
+      base_options, max_results=_MAX_RESULTS)
+
+    # Classifies the input.
+    text_result = classifier.classify(_INPUT_STR)
+    categories = text_result.classifications[0].categories
+
+    self.assertLessEqual(
+      len(categories), _MAX_RESULTS, 'Too many results returned.')
+
+  def test_score_threshold_option(self):
+    # Creates classifier.
+    base_options = _BaseOptions(file_name=self.model_path)
+
+    classifier = _create_classifier_from_options(
+      base_options, score_threshold=_SCORE_THRESHOLD)
+
+    # Classifies the input.
+    text_result = classifier.classify(_INPUT_STR)
+    categories = text_result.classifications[0].categories
+
+    for category in categories:
+      self.assertGreaterEqual(
+        category.score, _SCORE_THRESHOLD,
+        f'Classification with score lower than threshold found. {category}')
+
+  def test_allowlist_option(self):
+    # Creates classifier.
+    base_options = _BaseOptions(file_name=self.model_path)
+
+    classifier = _create_classifier_from_options(
+      base_options, category_name_allowlist=_ALLOW_LIST)
+
+    # Classifies the input.
+    text_result = classifier.classify(_INPUT_STR)
+    categories = text_result.classifications[0].categories
+
+    for category in categories:
+      label = category.category_name
+      self.assertIn(label, _ALLOW_LIST,
+                    f'Label {label} found but not in label allow list')
+
+  def test_denylist_option(self):
+    # Creates classifier.
+    base_options = _BaseOptions(file_name=self.model_path)
+
+    classifier = _create_classifier_from_options(
+      base_options, score_threshold=0.01, category_name_denylist=_DENY_LIST)
+
+    # Classifies the input.
+    text_result = classifier.classify(_INPUT_STR)
+    categories = text_result.classifications[0].categories
+
+    for category in categories:
+      label = category.category_name
+      self.assertNotIn(label, _DENY_LIST,
+                       f'Label {label} found but in deny list.')
+
+  def test_combined_allowlist_and_denylist(self):
+    # Fails with combined allowlist and denylist
+    with self.assertRaisesRegex(
+        ValueError,
+        r'`class_name_allowlist` and `class_name_denylist` are mutually '
+        r'exclusive options.'):
+      base_options = _BaseOptions(file_name=self.model_path)
+      classification_options = classification_options_pb2.ClassificationOptions(
+        category_name_allowlist=['foo'], category_name_denylist=['bar'])
+      options = _NLClassifierOptions(
+        base_options=base_options,
+        classification_options=classification_options)
+      _NLClassifier.create_from_options(options)
 
 
 if __name__ == "__main__":
