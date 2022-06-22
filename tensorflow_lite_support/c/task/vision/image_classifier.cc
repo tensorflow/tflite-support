00d8993c8fc7a12a35e2255a7f29faa100c09b5a
Prianka Liz Kariat <prianka.kariat@ymedialabs.com>
2022-06-22 23:56:58 +0530

    Updated files

--- tensorflow_lite_support/c/task/vision/image_classifier.cc
+++ tensorflow_lite_support/c/task/vision/image_classifier.cc
@@ -88,16 +88,16 @@ StatusOr<ImageClassifierOptionsCpp> CreateImageClassifierCppOptionsFromCOptions(
   cpp_options.set_score_threshold(
       c_options->classification_options.score_threshold);
 
-  cpp_options.mutable_base_options()
-      ->mutable_compute_settings()
-      ->mutable_tflite_settings()
-      ->set_delegate(tflite::proto::Delegate::CORE_ML);
+  // cpp_options.mutable_base_options()
+  //     ->mutable_compute_settings()
+  //     ->mutable_tflite_settings()
+  //     ->set_delegate(tflite::proto::Delegate::CORE_ML);
 
-  cpp_options.mutable_base_options()
-      ->mutable_compute_settings()
-      ->mutable_tflite_settings()
-      ->mutable_coreml_settings()
-      ->set_enabled_devices(tflite::proto::CoreMLSettings::DEVICES_ALL);
+  // cpp_options.mutable_base_options()
+  //     ->mutable_compute_settings()
+  //     ->mutable_tflite_settings()
+  //     ->mutable_coreml_settings()
+  //     ->set_enabled_devices(tflite::proto::CoreMLSettings::DEVICES_ALL);
 
   return cpp_options;
 }
