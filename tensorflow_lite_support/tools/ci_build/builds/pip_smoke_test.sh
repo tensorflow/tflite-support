652063c7ec83f3128e8100abf81e9c4bcb1f9cfd
A. Unique TensorFlower <gardener@tensorflow.org>
2022-08-17 13:39:42 -0700

    Exclude Windows from verification check for Task Library
    PiperOrigin-RevId: 468282010

--- tensorflow_lite_support/tools/ci_build/builds/pip_smoke_test.sh
+++ tensorflow_lite_support/tools/ci_build/builds/pip_smoke_test.sh
@@ -61,7 +61,10 @@ function run_smoke_test() {
 
   test_codegen
 
-  test_tfl_task_lib
+  # On Mac and Ubuntu, verify that the task library builds successfully.
+  if [[ "$OSTYPE" != "msys" ]]; then
+    test_tfl_task_lib
+  fi
 
   # Deactivate from virtualenv.
   deactivate || source deactivate || \
