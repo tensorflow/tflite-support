569efa37f5f12d23d280902f6f1799114b67bd7d
Dmitri Gribenko <dmitrig@google.com>
2024-01-03 06:27:43 -0800

    Internal Code Change
    PiperOrigin-RevId: 595384829

--- tensorflow_lite_support/cc/text/tokenizers/bert_tokenizer.cc
+++ tensorflow_lite_support/cc/text/tokenizers/bert_tokenizer.cc
@@ -15,6 +15,8 @@ limitations under the License.
 
 #include "tensorflow_lite_support/cc/text/tokenizers/bert_tokenizer.h"
 
+#include <cstdint>
+
 #include "tensorflow_lite_support/cc/port/integral_types.h"
 
 namespace tflite {
@@ -67,8 +69,8 @@ WordpieceTokenizerResult BertTokenizer::TokenizeWordpiece(
   std::vector<int>& wp_absolute_end_offset = result.wp_end_offset;
 
   std::vector<absl::string_view> tokens;
-  std::vector<int64> begin_offsets;
-  std::vector<int64> end_offsets;
+  std::vector<int64_t> begin_offsets;
+  std::vector<int64_t> end_offsets;
 
   // Run through tokenize function
   tensorflow::text::RegexSplit(input, delim_re_, true, include_delim_re_,
