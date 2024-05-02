421ba8ad9afbf21c418e098f2aa7feeab490f92e
Samya Bose <73535580+samyabose@users.noreply.github.com>
2024-05-02 23:32:41 +0530

    additional builds for base task, task vision and odml image

--- build.sh
+++ build.sh
@@ -3,3 +3,8 @@ bazel build --copt=-w \
         --cxxopt=-std=c++17 \
         --fat_apk_cpu=arm64-v8a \
         tensorflow_lite_support/java:tensorflowlite_support_java.aar
+
+bazel build --fat_apk_cpu=arm64-v8a tensorflow_lite_support/odml/java/image
+bazel build --fat_apk_cpu=arm64-v8a tensorflow_lite_support/metadata/java:tensorflow-lite-support-metadata-lib
+bazel build --fat_apk_cpu=arm64-v8a //tensorflow_lite_support/java/src/java/org/tensorflow/lite/task/core:base-task-api
+bazel build --fat_apk_cpu=arm64-v8a //tensorflow_lite_support/java/src/java/org/tensorflow/lite/task/vision:task-library-vision
