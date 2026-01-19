86873eb480828f53793cbe8ce21d5b11173eca7e
A. Unique TensorFlower <gardener@tensorflow.org>
2026-01-19 11:50:17 -0800

    Add missing include for <string> in zip_writable_mem_file.h
    PiperOrigin-RevId: 858252877

--- tensorflow_lite_support/metadata/cc/utils/zip_writable_mem_file.h
+++ tensorflow_lite_support/metadata/cc/utils/zip_writable_mem_file.h
@@ -17,6 +17,7 @@ limitations under the License.
 #define TENSORFLOW_LITE_SUPPORT_METADATA_CC_UTILS_ZIP_MEM_FILE_H_
 
 #include <cstdlib>
+#include <string>
 
 #include "absl/strings/string_view.h"  // from @com_google_absl
 #include "contrib/minizip/ioapi.h"
