0cfe31372887c85ba84987c3a4451910fa323bc8
Nick Desaulniers <ndesaulniers@google.com>
2026-07-24 14:33:11 -0700

    jni_utils: IWYU for recent libc++
    libc++ is getting stricter about its transitive includes.  IWYU to avoid
    errors around malloc being undefined.

--- tensorflow_lite_support/cc/utils/jni_utils.cc
+++ tensorflow_lite_support/cc/utils/jni_utils.cc
@@ -16,6 +16,7 @@ limitations under the License.
 #include "tensorflow_lite_support/cc/utils/jni_utils.h"
 
 #include <dlfcn.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "absl/memory/memory.h"  // from @com_google_absl
